21eb075ff16a1ac8829f238ad1d2fed7a738d2c6
Robert M Ricci <rmricci121@gmail.com>
2020-02-22 01:46:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title FROM projects
-   JOIN pledges ON pledges.project_id = projects.id
+   JOIN pledges, amount ON pledges.project_id = projects.id
    ORDER BY projects.title"
 end
 
